0c59d04a8d1ea4da95321e9aebd4e8bcadf4b402
EG <fangweiyi@gdu-tech.com>
2017-08-04 16:57:31 +0800

    Initial workspace~

---
+++ Widgets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Widgets
+//
+//  Created by EG on 2017/8/4.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Widgets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Widgets
+//
+//  Created by EG on 2017/8/4.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
